38edaf4251fad8dff1184d90d2adb38caa67970c
lacerdafh <140887060+lacerdafh@users.noreply.github.com>
2024-11-26 23:05:21 -0300

    Update app.py

--- app/app.py
+++ app/app.py
@@ -75,119 +75,117 @@ def initialize_vector_store() -> FAISS:
 
 
 def get_chat_response(context: List[Document], question: str) -> str:
-
     examples = [
-        {
-            "pergunta": "Como posso utilizar morfina para dor?",
-            "resposta": """
-    Pergunta de acompanhamento necessária: Sim.
-    1. Pergunta de acompanhamento: Existe referência a algum capítulo no texto?
-       Resposta intermediária: Sim.
-    2. Pergunta de acompanhamento: Qual capítulo é relevante?
-       Resposta intermediária: Dor.
-    3. Pergunta de acompanhamento: Dentro do capítulo 'Dor', existe referência a algum subtítulo?
-       Resposta intermediária: Sim.
-    4. Pergunta de acompanhamento: Qual subtítulo?
-       Resposta intermediária: Morfina.
-    
-    Resposta final:
-    Segundo o Manual de Cuidados Paliativos, 2ª ed.:
-    Para dor, a morfina pode ser utilizada da seguinte forma:
-    Morfina simples:
-    - Dose inicial: 5 mg a cada 4 horas (VO), com necessidade de avaliar doses mais baixas em pacientes idosos, com disfunção renal ou hepática;
-    - Dose máxima: Não possui dose teto; o limite é o efeito colateral, devendo ser titulado cuidadosamente;
-    - Frequência de administração: A cada 4 horas. Em casos específicos (idosos, disfunções), considerar a cada 6 horas;
-    - Vias de administração: Oral, sonda nasoenteral, gastrostomia, endovenosa, subcutânea, hipodermóclise;
-    - Equipotência: Morfina endovenosa é três vezes mais potente que a oral;
-    - Particularidades: Metabolizada no fígado e excretada pelo rim. Usar com cautela em pacientes com doença hepática ou renal;
-    - Disponibilidade no SUS: Constante na Rename 2022.
-    """,
-        },
-        {
-            "pergunta": "Quais são os efeitos colaterais da morfina?",
-            "resposta": """
-    Pergunta de acompanhamento necessária: Sim.
-    1. Existe um capítulo relacionado? Sim.
-       Resposta intermediária: Dor.
-    2. Algum subtítulo é relevante? Sim.
-       Resposta intermediária: Efeitos colaterais.
-    
-    Resposta final:
-    Segundo o Manual de Cuidados Paliativos, 2ª ed.:
-    Os principais efeitos colaterais da morfina incluem náuseas, vômitos, constipação, sonolência e depressão respiratória. Esses efeitos devem ser monitorados e manejados adequadamente.
-    """
-        },
-        {
-            "pergunta": "Quem são os autores do Capitulo de Dor?",
-            "resposta": """
-    Pergunta de acompanhamento necessária: Sim.
-    1. Existe um capítulo relacionado? Sim.
-       Resposta intermediária: Dor.
-    2. Algum subtítulo é relevante? Não.
-       Resposta intermediária: Efeitos colaterais.
-    3. Foi perguntado sobre nomes de pessoas? Sim.
-       Resposta intermediária: Sim.
-    4. É sobre o livro ou capítulo?
-       Resposta intermediária: capítulo.
-    
-    
-    Resposta final:
-    Os autores do Capítulo de Dor do Manual de Cuidados Paliativos, 2ª ed., são:
-    1- Daniel Felgueiras Rolo
-    2- Maria Perez Soares D'Alessandro
-    3- Gustavo Cassefo
-    4- Sergio Seiki Anagusko
-    5- Ana Paula Mirarchi Vieira Maiello
-    """
-        },
-        {
-            "pergunta": "Quem são os autores do livro?",
-            "resposta": """
-    Pergunta de acompanhamento necessária: Sim.
-    1. Existe um capítulo relacionado? 
-       Resposta intermediária: Não.
-    2. Algum subtítulo é relevante? 
-       Resposta intermediária: Não.
-    3. Foi perguntado sobre nomes de pessoas? Sim.
-       Resposta intermediária: Sim.
-    4. É sobre o livro ou capítulo?
-       Resposta intermediária: livro.
-    
-    
-    Resposta final:
-    A equipe responsável pelo Manual de Cuidados Paliativos, 2ª ed., foram:
-    
-    Editores
-    Maria Perez Soares D’Alessandro, Lara Cruvinel Barbosa, Sergio Seiki Anagusko, Ana Paula Mirarchi Vieira
-    Maiello, Catherine Moreira Conrado, Carina Tischler Pires e Daniel Neves Forte.
-    
-    Autores
-    Aline de Almada Messias, Ana Cristina Pugliese de Castro, Ana Paula Mirarchi Vieira Maiello, Caroline
-    Freitas de Oliveira, Catherine Moreira Conrado, Daniel Felgueiras Rolo, Fábio Holanda Lacerda, Fernanda
-    Pimentel Coelho, Fernanda Spiel Tuoto, Graziela de Araújo Costa , Gustavo Cassefo, Heloisa Maragno,
-    Hieda Ludugério de Souza, Lara Cruvinel Barbosa, Leonardo Bohner Hoffmann, Lícia Maria Costa Lima,
-    Manuele de Alencar Amorim, Marcelo Oliveira Silva, Maria Perez Soares D’Alessandro, Mariana Aguiar
-    Bezerra, Nathalia Maria Salione da Silva, Priscila Caccer Tomazelli, Sergio Seiki Anagusko e Sirlei Dal Moro.
-    
-    Equipe da Secretaria de Atenção Especializada em Saúde do Ministério da Saúde
-    Nilton Pereira Junior, Mariana Borges Dias, Taís Milene Santos de Paiva, Cristiane Maria Reis Cristalda e
-    Lorayne Andrade Batista.
-    
-    Equipe do CONASS
-    René José Moreira dos Santos, Eliana Maria Ribeiro Dourado e Luciana Toledo.
-    
-    Equipe de apoio HSL:
-    Guilherme Fragoso de Mello, Juliana de Lima Gerarduzzi e Luiz Felipe Monteiro Correia.
-    """
-        }
-    ]
-    """Processa a pergunta e retorna a resposta do modelo usando FewShotChatMessagePromptTemplate."""
+                {
+                    "pergunta": "Como posso utilizar morfina para dor?",
+                    "resposta": """
+            Pergunta de acompanhamento necessária: Sim.
+            1. Pergunta de acompanhamento: Existe referência a algum capítulo no texto?
+            Resposta intermediária: Sim.
+            2. Pergunta de acompanhamento: Qual capítulo é relevante?
+            Resposta intermediária: Dor.
+            3. Pergunta de acompanhamento: Dentro do capítulo 'Dor', existe referência a algum subtítulo?
+            Resposta intermediária: Sim.
+            4. Pergunta de acompanhamento: Qual subtítulo?
+            Resposta intermediária: Morfina.
+
+            Resposta final:
+            Segundo o Manual de Cuidados Paliativos, 2ª ed.:
+            Para dor, a morfina pode ser utilizada da seguinte forma:
+            Morfina simples:
+            - Dose inicial: 5 mg a cada 4 horas (VO), com necessidade de avaliar doses mais baixas em pacientes idosos, com disfunção renal ou hepática;
+            - Dose máxima: Não possui dose teto; o limite é o efeito colateral, devendo ser titulado cuidadosamente;
+            - Frequência de administração: A cada 4 horas. Em casos específicos (idosos, disfunções), considerar a cada 6 horas;
+            - Vias de administração: Oral, sonda nasoenteral, gastrostomia, endovenosa, subcutânea, hipodermóclise;
+            - Equipotência: Morfina endovenosa é três vezes mais potente que a oral;
+            - Particularidades: Metabolizada no fígado e excretada pelo rim. Usar com cautela em pacientes com doença hepática ou renal;
+            - Disponibilidade no SUS: Constante na Rename 2022.
+            """,
+                },
+                {
+                    "pergunta": "Quais são os efeitos colaterais da morfina?",
+                    "resposta": """
+            Pergunta de acompanhamento necessária: Sim.
+            1. Existe um capítulo relacionado? Sim.
+            Resposta intermediária: Dor.
+            2. Algum subtítulo é relevante? Sim.
+            Resposta intermediária: Efeitos colaterais.
+
+            Resposta final:
+            Segundo o Manual de Cuidados Paliativos, 2ª ed.:
+            Os principais efeitos colaterais da morfina incluem náuseas, vômitos, constipação, sonolência e depressão respiratória. Esses efeitos devem ser monitorados e manejados adequadamente.
+            """
+                },
+                {
+                    "pergunta": "Quem são os autores do Capitulo de Dor?",
+                    "resposta": """
+            Pergunta de acompanhamento necessária: Sim.
+            1. Existe um capítulo relacionado? Sim.
+            Resposta intermediária: Dor.
+            2. Algum subtítulo é relevante? Não.
+            Resposta intermediária: Efeitos colaterais.
+            3. Foi perguntado sobre nomes de pessoas? Sim.
+            Resposta intermediária: Sim.
+            4. É sobre o livro ou capítulo?
+            Resposta intermediária: capítulo.
+
+
+            Resposta final:
+            Os autores do Capítulo de Dor do Manual de Cuidados Paliativos, 2ª ed., são:
+            1- Daniel Felgueiras Rolo
+            2- Maria Perez Soares D'Alessandro
+            3- Gustavo Cassefo
+            4- Sergio Seiki Anagusko
+            5- Ana Paula Mirarchi Vieira Maiello
+            """
+                },
+                {
+                    "pergunta": "Quem são os autores do livro?",
+                    "resposta": """
+            Pergunta de acompanhamento necessária: Sim.
+            1. Existe um capítulo relacionado? 
+            Resposta intermediária: Não.
+            2. Algum subtítulo é relevante? 
+            Resposta intermediária: Não.
+            3. Foi perguntado sobre nomes de pessoas? Sim.
+            Resposta intermediária: Sim.
+            4. É sobre o livro ou capítulo?
+            Resposta intermediária: livro.
+
+
+            Resposta final:
+            A equipe responsável pelo Manual de Cuidados Paliativos, 2ª ed., foram:
+
+            Editores
+            Maria Perez Soares D’Alessandro, Lara Cruvinel Barbosa, Sergio Seiki Anagusko, Ana Paula Mirarchi Vieira
+            Maiello, Catherine Moreira Conrado, Carina Tischler Pires e Daniel Neves Forte.
+
+            Autores
+            Aline de Almada Messias, Ana Cristina Pugliese de Castro, Ana Paula Mirarchi Vieira Maiello, Caroline
+            Freitas de Oliveira, Catherine Moreira Conrado, Daniel Felgueiras Rolo, Fábio Holanda Lacerda, Fernanda
+            Pimentel Coelho, Fernanda Spiel Tuoto, Graziela de Araújo Costa , Gustavo Cassefo, Heloisa Maragno,
+            Hieda Ludugério de Souza, Lara Cruvinel Barbosa, Leonardo Bohner Hoffmann, Lícia Maria Costa Lima,
+            Manuele de Alencar Amorim, Marcelo Oliveira Silva, Maria Perez Soares D’Alessandro, Mariana Aguiar
+            Bezerra, Nathalia Maria Salione da Silva, Priscila Caccer Tomazelli, Sergio Seiki Anagusko e Sirlei Dal Moro.
+
+            Equipe da Secretaria de Atenção Especializada em Saúde do Ministério da Saúde
+            Nilton Pereira Junior, Mariana Borges Dias, Taís Milene Santos de Paiva, Cristiane Maria Reis Cristalda e
+            Lorayne Andrade Batista.
+
+            Equipe do CONASS
+            René José Moreira dos Santos, Eliana Maria Ribeiro Dourado e Luciana Toledo.
+
+            Equipe de apoio HSL:
+            Guilherme Fragoso de Mello, Juliana de Lima Gerarduzzi e Luiz Felipe Monteiro Correia.
+            """
+                }
+            ]
     try:
-        groq_key, * = load_api_keys()
+        groq_key, _ = load_api_keys()
         chat_model = ChatGroq(
             api_key=groq_key,
             model_name="llama-3.2-3b-preview",
-            temperature=0.5,
+            temperature=0.3,
             max_tokens=1028
         )
 
